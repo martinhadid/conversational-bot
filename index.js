8be27936be2fa06356ae8d287eeb0f09a121c803
martinhadid <martinhadid@gmail.com>
2020-06-22 00:32:31 +0300

    add port and fallback

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const app = express();
 app.use(express.static(__dirname + '/views')); // html
 app.use(express.static(__dirname + '/public')); // js, css, images
 
-const server = app.listen(5000);
+const port = process.env.PORT || 3000;
+const server = app.listen(port);
 app.get('/', (req, res) => {
   res.sendFile('index.html');
 });
